e3d0f4260da76a5a45d378e201fd182da69b5842
kevinSelles <kevin.selles.c@gmail.com>
2025-06-20 14:11:38 +0200

    Create ejercicio5.js

---
+++ ejercicio5.js
@@ -0,0 +1,65 @@
+//5.1 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+//con los valores que sean mayor que 18
+const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+
+const adults = ages.filter(age => age > 18);
+
+console.log(adults);
+
+//5.2 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+//con los valores que sean par.
+const ages2 = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+
+const evenAges = ages2.filter(age => age % 2 === 0);
+
+console.log(evenAges);
+
+//5.3 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+//con los streamers que tengan el gameMorePlayed = 'League of Legends'.
+const streamers = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'}, 
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+
+const streamersLOL = streamers.filter(streamer => streamer.gameMorePlayed === 'League of Legends');
+
+console.log(streamersLOL);
+
+//5.4 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+//con los streamers que incluyan el caracter 'u' en su propiedad .name. Recomendamos 
+//usar la funcion .includes() para la comprobación.
+const streamers2 = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+
+const streamersWithU = streamers2.filter(streamer => {
+
+  if(streamer.name.includes('u'))
+    return streamer;
+})
+
+console.log(streamersWithU);
+
+//5.5 utiliza .filter() para generar un nuevo array con los streamers que incluyan 
+//el caracter 'Legends' en su propiedad .gameMorePlayed. Recomendamos usar la funcion 
+//.includes() para la comprobación.
+//Además, pon el valor de la propiedad .gameMorePlayed a MAYUSCULAS cuando 
+//.age sea mayor que 35.
+
+const streamersLegend = streamers.filter(streamer => {
+
+  if(streamer.gameMorePlayed.includes('Legend')) {
+    if(streamer.age > 35) {
+    streamer.gameMorePlayed = streamer.gameMorePlayed.toUpperCase();
+  }
+  return true;
+}
+  return false;
+})
+
+console.log(streamersLegend);
