33c3960a485302c333fafc533930a9bda0fca8d7
kevinSelles <kevin.selles.c@gmail.com>
2025-06-20 12:13:57 +0200

    Create ejercicio3.js

---
+++ ejercicio3.js
@@ -0,0 +1,34 @@
+//3.1 Dado el siguiente array, crea una copia usando spread operators.
+const pointsList = [32, 54, 21, 64, 75, 43];
+
+const copiaPointsList = [...pointsList];
+console.log(copiaPointsList);
+
+//3.2 Dado el siguiente objeto, crea una copia usando spread operators.
+const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+
+const copiaToy = {...toy};
+console.log(copiaToy);
+
+//3.3 Dado los siguientes arrays, crea un nuevo array juntandolos usando 
+//spread operatos.
+const pointList = [32, 54, 21, 64, 75, 43];
+const pointList2 = [54,87,99,65,32];
+
+const pointListConjunta = [...pointList, ...pointList2];
+console.log(pointListConjunta);
+
+//3.4 Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos 
+//con spread operators.
+const toy2 = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+const toyUpdate = {lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk']}
+
+const finalToy = {...toy2, ...toyUpdate};
+console.log(finalToy);
+
+//3.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2 
+//pero sin editar el array inicial. De nuevo, usando spread operatos.
+const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
+
+const copyColors = [...colors.slice(0, 2), ...colors.slice(3)];
+console.log(copyColors);
